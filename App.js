c5002709288a270902328a60da7390b66e91f171
tamaraeaton <teaton@banyanlabs.io>
2021-04-13 11:10:45 -0700

    Image Picker Working - Stuck at URI/Base64

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React, {useState} from 'react';
 import {Text,View, Image, StyleSheet, Button} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const App = () => {
   const [avatar,setAvatar] = useState(null)
 
   const getAvatar = () => {
-    ImagePicker.showImagePicker({}, response => {
-      setAvatar(response.uri);
-    })
+
+  launchImageLibrary({}, response => {
+    // if(response.didCancel) {
+    //   console.log('user canceled')
+    // } else if (response.error) {
+    //   console.log (response.error)
+    // } else {
+    //   console.log(response.uri)
+    //    setAvatar(response.uri);
+    // }
+    const source = { uri: "data:image/jpeg;base64," + response.data}
+    console.log(source)
+   setAvatar(response.uri);
+  })
+
+    // ImagePicker.showImagePicker({}, response => {
+    //   setAvatar(response.uri);
+    // })
   }
 
   return (
