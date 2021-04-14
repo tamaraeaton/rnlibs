0bdbfc1f4172d198288d074ca677509e4b9b578d
tamaraeaton <teaton@banyanlabs.io>
2021-04-14 08:36:38 -0700

    Up to #41@11:27 contacts returns empty array

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React, {useState} from 'react';
-import {Text,View, Image, StyleSheet, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text, View, 
+  Image, StyleSheet, 
+  Button, PermissionsAndroid
+} from 'react-native';
 import Contacts from 'react-native-contacts';
 
 import Picker from './src/picker'
 
 const App = () => {
+
+  useEffect(()=>{
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      {
+        'title': 'Contacts',
+        'message': 'This app would like to view your contacts.',
+        'buttonPositive': 'Please accept bare mortal'
+      }
+    )
+.then(()=>{
+  Contacts.getAll().then(contacts=>{
+    console.log(contacts)
+  }).catch(err=>console.log(err))
+})
+  },[])
 
   return (
     <View>
